8cb36b32e1d07cc5a1c099a9c8ddc1100172d518
hlzhl <124333308+hlzhl@users.noreply.github.com>
2025-06-26 01:45:53 +0800

    Update xvideos.sh

--- xvideos.sh
+++ xvideos.sh
@@ -21,8 +21,8 @@ rm -rf a
 # -c:a aac -b:a 128k -movflags +faststart \
 # "$name.mp4"
 
-ffmpeg -loglevel error -threads 4 -i "$final_m3m8"\
--c:v copy -c:a copy \  # 直接复制音视频流，跳过编码
--movflags +faststart \  # 将索引放在文件头部
+ffmpeg -loglevel error -threads 4 -i "$final_m3m8" \
+-c:v copy -c:a copy \
+-movflags +faststart \
 "$name.mp4"
 
